9ad36339bc7de6333a88ba7510b0310cd2cee8de
Tydre1 <oliveiraandre945@gmail.com>
2023-08-06 11:52:26 -0300

    arrumando coisas da novaTecnologia

--- fluxo.js
+++ fluxo.js
@@ -13,43 +13,33 @@ while (true) {
 let linguagem = "";
 let ferramenta = "";
 
-if(area.toLowerCase() === "front-end") {
+if (area.toLowerCase() === "front-end") {
     linguagem = prompt("Você quer aprender React ou JavaScript?");
     ferramenta = prompt("Você também precisa aprender HTML ou CSS. Digite apenas uma delas.");
     alert(`Certo! ${linguagem} e ${ferramenta}.`);
-
 } else if (area.toLowerCase() === "back-end") {
     linguagem = prompt("Você quer aprender C#, Java, Ruby ou Python?");
     alert(`Certo! ${linguagem}`);
-    
 } else {
-    alert("Você não inseriu uma área valida!");
-}
-
-const especialidadeOuFullstack = parseInt(prompt ("Digite 1 para seguir se especializando na área desejada ou 2 para seguir se desenvolvendo para se tornar Fullstack"));
-if(especialidadeOuFullstack === 1) {
-    alert(`Continue se especializando em ${linguagem} e ${ferramenta} para dominar a área ${area}!`);
-}
-else if (especialidadeOuFullstack === 2) {
-    alert(`Chegou a hora de começar a aprender outras linguagens além de ${linguagem} se você quer se tornar Fullstack!`);
-}
-else {
-    alert("Você não inseriu um valor válido!");
+    alert("Você não inseriu uma área válida!");
 }
 
 let msg = prompt("Você gostaria de aprender mais uma tecnologia? Digite 'ok' para confirmar.");
+
+let novaTecnologia
+
 while (msg.toLowerCase() === 'ok'){
-    let novaTecnologia = prompt("Qual?");
+    novaTecnologia = prompt("Qual?");
     alert(`${novaTecnologia} é realmente uma tecnologia muito legal!`);
     msg = prompt("Você gostaria de aprender mais uma tecnologia? Digite 'ok' para confirmar.");
 }
 
-function exibirMensagemHTML(mensagem) {
-    document.getElementById("mensagem").textContent = mensagem;
+let mensagemFinal = "";
+
+if (area.toLowerCase() === "front-end") {
+    mensagemFinal = `Muito bem! Você escolheu as tecnologias ${linguagem} e ${ferramenta}. Irei disponibilizar para você as documentações e os cursos. Boa Sorte na sua Jornada.`;
+} else if (area.toLowerCase() === "back-end") {
+    mensagemFinal = `Muito bem! Você escolheu a tecnologia ${linguagem} e ${novaTecnologia}. Irei disponibilizar para você as documentações e os cursos. Boa sorte na sua Jornada!`;
 }
 
-if (linguagem.toLowerCase() === "front-end") {
-    exibirMensagemHTML(`Muito bem! Você escolheu as tecnologias ${linguagem} e ${ferramenta}. Irei disponibilizar para você as documentações e os cursos. Boa Sorte na sua Jornada.`);
-} else if (linguagem.toLowerCase() === "back-end") {
-    exibirMensagemHTML(`Muito bem! Você escolheu a tecnologia ${linguagem}. Irei disponibilizar para você as documentações e os cursos. Boa Sorte na sua Jornada.`);
-}
+document.getElementById("mensagem").innerHTML = mensagemFinal;
